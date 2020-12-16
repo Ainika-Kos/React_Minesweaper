75056e6748dc6450118d421ebf60767307a32dc1
Ainika <aina.kostina@gmail.com>
2020-12-16 22:13:40 +0200

    Finished styling body and cells

--- my-app/src/components/App/index.tsx
+++ my-app/src/components/App/index.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { v4 as uuidv4 } from 'uuid';
 import NumberDisplay from '../NumberDisplay';
+import { generateCells } from '../../utils/index';
+import { Button } from '../Button/index';
 
 const App: React.FC = () => {
+  const [cells, setCells] = useState(generateCells());
+
+  const renderCells = (): React.ReactNode => {
+    return cells.map((row, rowIndex) =>
+      row.map((cell, colIndex) => <Button key={uuidv4()} />)
+    );
+  };
+
   return (
     <div className="App">
       <div className="Header">
         <NumberDisplay value={0} />
         <div className="Face">
-          <span role="img" aria-label="emojy-stars">🤩</span>
+          <span role="img" aria-label="emojy-stars">
+            🤩
+          </span>
         </div>
         <NumberDisplay value={23} />
       </div>
-      <div className="Body">Body</div>
+      <div className="Body">{renderCells()}</div>
     </div>
   );
 };
